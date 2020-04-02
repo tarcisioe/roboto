22ed487a8811c2cffad4a95fd6c481267178d42e
Tarcísio Eduardo Moreira Crocomo <tarcisioe@pm.me>
2020-04-02 20:16:21 -0300

    Add BotAPI.get_updates.

--- roboto/bot.py
+++ roboto/bot.py
@@ -1,9 +1,10 @@
 """The main bot class for Roboto."""
 from dataclasses import InitVar, dataclass, field
+from typing import List
 
 from .datautil import from_json
 from .http import HTTPMethod, make_request
-from .types import BotUser, Token
+from .types import BotUser, GetUpdatesRequest, Token, Update
 from .url import URL
 
 
@@ -31,6 +32,30 @@ class BotAPI:
         return from_json(
             BotUser, await make_request(self.api_url, HTTPMethod.GET, 'getMe')
         )
+
+    async def get_updates(
+        self,
+        offset: int = None,
+        limit: int = None,
+        timeout: int = None,
+        allowed_updates: List[str] = None,
+    ) -> List[Update]:
+        """getUpdates API method. Won't work if a webhook is setup.
+
+        Args:
+            offset: The offset to begin in the updates list.
+            limit: How many updates to get at maximum.
+            timeout: How long to long poll for.
+            allowed_updates: Which kind of updates to fetch.
+
+        Returns:
+            A list of Update objects.
+        """
+        request = GetUpdatesRequest(offset, limit, timeout, allowed_updates)
+        response = await make_request(
+            self.api_url, HTTPMethod.GET, 'getUpdates', request
+        )
+        return from_json(List[Update], response)
 
 
 __all__ = [
