e34e8f322fd28ae708ca72f4b584a20d855f7dcc
Tarcísio Eduardo Moreira Crocomo <tarcisioe@pm.me>
2020-04-05 21:12:30 -0300

    Streamline calls on bot.py.

--- roboto/bot.py
+++ roboto/bot.py
@@ -62,10 +62,11 @@ class BotAPI:
             A list of Update objects.
         """
         request = GetUpdatesRequest(offset, limit, timeout, allowed_updates)
-        response = await make_request(
-            self.api_url, HTTPMethod.GET, 'getUpdates', request
+
+        return from_json(
+            List[Update],
+            await make_request(self.api_url, HTTPMethod.GET, 'getUpdates', request),
         )
-        return from_json(List[Update], response)
 
     async def send_message(
         self,
@@ -90,10 +91,11 @@ class BotAPI:
             disable_notification,
             reply_to_message_id,
         )
-        response = await make_request(
-            self.api_url, HTTPMethod.POST, 'sendMessage', request
+
+        return from_json(
+            Message,
+            await make_request(self.api_url, HTTPMethod.POST, 'sendMessage', request),
         )
-        return from_json(Message, response)
 
 
 __all__ = [
