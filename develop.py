a091f73dac2cc97a96f3b3d01752de32033d14f8
Tarcísio Eduardo Moreira Crocomo <tarcisioe@pm.me>
2020-04-10 21:08:13 -0300

    Add pre-commit hooks and a contributing guide.

--- develop.py
+++ develop.py
@@ -2,20 +2,22 @@ from argparse import ArgumentParser
 from subprocess import run
 
 
-def develop(test_only: bool):
+def develop(test_only: bool, ci: bool):
     run(['poetry', 'install'], check=True)
     run(['poetry', 'run', 'pip', 'install', 'invoke'], check=True)
 
     if not test_only:
-        run(['poetry', 'run', 'inv', 'install-dev-tools'], check=True)
+        ci_args = ['--ci'] if ci else []
+        run(['poetry', 'run', 'inv', 'install-dev-tools', *ci_args], check=True)
 
 
 def main(args):
     argparser = ArgumentParser()
     argparser.add_argument('--test-only', action='store_true', default=False)
+    argparser.add_argument('--ci', action='store_true', default=False)
     ns = argparser.parse_args(args)
 
-    develop(ns.test_only)
+    develop(ns.test_only, ns.ci)
 
 
 if __name__ == '__main__':
