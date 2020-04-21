639e5e6450be7b8f84300535d109c256fc94187b
Tarcísio Eduardo Moreira Crocomo <tarcisioe@pm.me>
2020-04-21 03:14:29 -0300

    Ensure independent tasks run even if one of them fails.

--- tasks.py
+++ tasks.py
@@ -1,9 +1,12 @@
 """Linting module."""
 import shlex
 import subprocess
-from typing import List, Optional, Union
+from dataclasses import dataclass
+from functools import wraps
+from typing import Callable, List, Optional, Union, overload
 
 import typer
+from typing_extensions import Literal
 
 PACKAGE_NAME = 'roboto'
 
@@ -22,11 +25,77 @@ def command_output(command: Union[str, List[str]]) -> str:
     return process.stdout.decode('utf8')
 
 
-def execute(command: Union[str, List[str]]) -> None:
+@dataclass
+class CommandError:
+    """Represent that a given command failed."""
+
+    command_line: str
+    exit_code: int
+
+
+@dataclass
+class CommandSuccess:
+    """Represent that a given command ran successfully."""
+
+    command_line: str
+
+
+Result = Union[CommandError, CommandSuccess]
+
+
+def check_commands(f: Callable[..., List[Result]]) -> Callable[..., List[Result]]:
+    """Make a function that returns Results terminate the app if any of them failed."""
+
+    @wraps(f)
+    def _inner(*args, **kwargs) -> List[Result]:
+        results = f(*args, **kwargs)
+
+        failed_results = [r for r in results if isinstance(r, CommandError)]
+
+        if failed_results:
+            for failed in failed_results:
+                print(
+                    f'Command "{failed.command_line}" failed with error code '
+                    f'{failed.exit_code}.'
+                )
+            raise typer.Exit(code=1)
+
+        return results
+
+    return _inner
+
+
+@overload
+def execute(
+    command: Union[str, List[str]], *, raise_error: Literal[True] = True
+) -> CommandSuccess:
+    """Overload for when raise_error is True.
+
+    In this case, we never return CommandError (we raise the subprocess
+    exception)."""
+
+
+@overload
+def execute(command: Union[str, List[str]], *, raise_error: Literal[False]) -> Result:
+    """Overload for when raise_error is True.
+
+    In this case, we never raise, and instead we return CommandError."""
+
+
+def execute(command: Union[str, List[str]], *, raise_error: bool = True) -> Result:
     """Echo and run a command."""
     command_str = command if isinstance(command, str) else ' '.join(command)
     print(f'### Executing: {command_str}')
-    run_command(command)
+
+    try:
+        run_command(command)
+    except subprocess.CalledProcessError as e:
+        if raise_error:
+            raise
+
+        return CommandError(command_str, e.returncode)
+
+    return CommandSuccess(command_str)
 
 
 EVERYTHING = [
@@ -61,14 +130,13 @@ def install_dev_tools(
             'pylint',
             'pylint-quotes',
             *extra_deps,
-        ]
+        ],
     )
 
     if not ci:
         execute('pre-commit install')
 
 
-@APP.command()
 def test(
     coverage: bool = typer.Option(  # noqa: B008
         default=False, help='Generate coverage information.'
@@ -76,18 +144,26 @@ def test(
     html: bool = typer.Option(  # noqa: B008
         default=False, help='Generate an html coverage report.'
     ),
-):
+) -> List[Result]:
     """Run tests."""
     coverage_flag = [f'--cov={PACKAGE_NAME}'] if coverage else []
 
-    execute(['pytest', *coverage_flag, 'tests'])
+    return [
+        execute(['pytest', *coverage_flag, 'tests'], raise_error=False),
+        *(coverage_html() if coverage and html else ()),
+    ]
 
-    if coverage and html:
-        coverage_html()
+
+APP.command()(check_commands(test))
 
 
-@APP.command()
-def lint(files: Optional[List[str]] = typer.Argument(default=None,)):  # noqa: B008
+def lint(
+    files: Optional[List[str]] = typer.Argument(default=None,),  # noqa: B008
+    *,
+    full_report: bool = typer.Option(  # noqa: B008
+        default=True, help='Print detailed reports.'
+    ),
+) -> List[Result]:
     """Run all linters.
 
     If files is omitted. everything is linted.
@@ -95,18 +171,24 @@ def lint(files: Optional[List[str]] = typer.Argument(default=None,)):  # noqa: B
 
     subject = files if files else EVERYTHING
 
-    execute(['mypy', *subject])
-    execute(['flake8', *subject])
-    execute(['pylint', *subject])
+    pylint_reports = ['-r', 'y'] if full_report else ['-r', 'n']
+
+    return [
+        execute(['mypy', *subject], raise_error=False),
+        execute(['flake8', *subject], raise_error=False),
+        execute(['pylint', *pylint_reports, *subject], raise_error=False),
+    ]
 
 
-@APP.command()
+APP.command()(check_commands(lint))
+
+
 def format(  # pylint: disable=redefined-builtin
     files: Optional[List[str]] = typer.Argument(default=None),  # noqa: B008
     check: bool = typer.Option(  # noqa: B008
         default=False, help='Only checks instead of modifying.'
     ),
-):
+) -> List[Result]:
     """Run all formatters.
 
     If files is omitted. everything is linted.
@@ -117,28 +199,47 @@ def format(  # pylint: disable=redefined-builtin
 
     subject = files if files else EVERYTHING
 
-    execute(['black', '-q', *black_check_flag, *subject])
-    execute(['isort', '-rc', '-y', '-q', *isort_check_flag, *subject])
+    return [
+        execute(['black', '-q', *black_check_flag, *subject], raise_error=False),
+        execute(
+            ['isort', '-rc', '-y', '-q', *isort_check_flag, *subject], raise_error=False
+        ),
+    ]
 
 
-@APP.command()
+APP.command()(check_commands(format))
+
+
 def coverage_html():
     """Generate an html coverage report."""
-    execute(f'coverage html')
+    return [
+        execute(f'coverage html', raise_error=False),
+    ]
 
 
-@APP.command()
-def static_checks():
+APP.command()(check_commands(coverage_html))
+
+
+def static_checks() -> List[Result]:
     """Run all static checks over all code."""
-    lint([])
-    format([], check=True)
+    return [
+        *lint([]),
+        *format([], check=True),
+    ]
 
 
-@APP.command()
-def all_checks():
+APP.command()(check_commands(static_checks))
+
+
+def all_checks() -> List[Result]:
     """Run all checks (static checks and tests) over all code."""
-    static_checks()
-    test()
+    return [
+        *static_checks(),
+        *test(),
+    ]
+
+
+APP.command()(check_commands(all_checks))
 
 
 if __name__ == '__main__':
