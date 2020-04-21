e9e44933b6464c6a754a8debd0e1247d59832b73
Tarcísio Eduardo Moreira Crocomo <tarcisioe@pm.me>
2020-04-21 02:02:57 -0300

    Use typer instead of invoke. Make pre-commit hooks faster.

--- develop.py
+++ develop.py
@@ -1,17 +1,23 @@
+"""Development bootstrap script."""
 from argparse import ArgumentParser
 from subprocess import run
 
 
 def develop(test_only: bool, ci: bool):
+    """Run the development bootstrap commands."""
     run(['poetry', 'install'], check=True)
-    run(['poetry', 'run', 'pip', 'install', 'invoke'], check=True)
+    run(['poetry', 'run', 'pip', 'install', 'typer'], check=True)
 
     if not test_only:
         ci_args = ['--ci'] if ci else []
-        run(['poetry', 'run', 'inv', 'install-dev-tools', *ci_args], check=True)
+        run(
+            ['poetry', 'run', 'python', '-m', 'tasks', 'install-dev-tools', *ci_args],
+            check=True,
+        )
 
 
 def main(args):
+    """Parse arguments and run script."""
     argparser = ArgumentParser()
     argparser.add_argument('--test-only', action='store_true', default=False)
     argparser.add_argument('--ci', action='store_true', default=False)
--- tasks.py
+++ tasks.py
@@ -1,47 +1,58 @@
-from dataclasses import dataclass, field
-from typing import List
+"""Linting module."""
+import shlex
+import subprocess
+from typing import List, Optional, Union
 
-from invoke import Result, task, UnexpectedExit
+import typer
 
-PACKAGE_NAME = "roboto"
-
-
-@dataclass
-class UnexpectedExits(UnexpectedExit):
-    failed_results: List[Result]
-
-    @dataclass
-    class _FakeResult:
-        hide: bool = field(default=True, init=False)
-        exited: int = field(default=1, init=False)
-
-    result: Result = field(default_factory=_FakeResult, init=False)
-
-    def __str__(self):
-        return '\n'.join(
-            '\n'.join(
-                (
-                    f'### Command \'{r.command}\' failed',
-                    str(r)
-                )
-            ) for r in self.failed_results
-        )
+PACKAGE_NAME = 'roboto'
 
 
-def check_all(results: List[Result]):
-    results = [result for result in results if result.exited != 0]
+def run_command(
+    command: Union[str, List[str]], *args, **kwargs
+) -> subprocess.CompletedProcess:
+    """Wrapper for subprocess.run to support passing the command as a string."""
+    split_command = shlex.split(command) if isinstance(command, str) else command
+    return subprocess.run(split_command, check=True, *args, **kwargs)
 
-    if results:
-        raise UnexpectedExits(results)
+
+def command_output(command: Union[str, List[str]]) -> str:
+    """Run a command and get its stdout."""
+    process = run_command(command, stdout=subprocess.PIPE)
+    return process.stdout.decode('utf8')
 
 
-@task
-def install_dev_tools(c, ci=False):
+def execute(command: Union[str, List[str]]) -> None:
+    """Echo and run a command."""
+    command_str = command if isinstance(command, str) else ' '.join(command)
+    print(f'### Executing: {command_str}')
+    run_command(command)
+
+
+EVERYTHING = [
+    'roboto',
+    'tests',
+    'develop.py',
+    'tasks.py',
+]
+APP = typer.Typer()
+
+
+@APP.command()
+def install_dev_tools(
+    ci: bool = typer.Option(  # noqa: B008
+        default=False, help='Avoid installing tools that are unneeded for CI jobs.'
+    )
+):
+    """Install development tools."""
     extra_deps = ['pre-commit'] if not ci else []
 
-    c.run(
-        ' '.join((
-            'poetry run pip install',
+    execute(
+        [
+            'poetry',
+            'run',
+            'pip',
+            'install',
             'black',
             'flake8',
             'flake8-bugbear',
@@ -49,70 +60,86 @@ def install_dev_tools(c, ci=False):
             'mypy',
             'pylint',
             'pylint-quotes',
-            *extra_deps
-        ))
+            *extra_deps,
+        ]
     )
 
     if not ci:
-        c.run('pre-commit install')
+        execute('pre-commit install')
 
 
-@task
-def test(c, coverage=False, html=False):
-    coverage_flag = f'--cov={PACKAGE_NAME}' if coverage else None
+@APP.command()
+def test(
+    coverage: bool = typer.Option(  # noqa: B008
+        default=False, help='Generate coverage information.'
+    ),
+    html: bool = typer.Option(  # noqa: B008
+        default=False, help='Generate an html coverage report.'
+    ),
+):
+    """Run tests."""
+    coverage_flag = [f'--cov={PACKAGE_NAME}'] if coverage else []
 
-    command = ' '.join(piece for piece in (
-        'pytest',
-        coverage_flag,
-        'tests'
-    ) if piece is not None)
-
-    c.run(command)
+    execute(['pytest', *coverage_flag, 'tests'])
 
     if coverage and html:
-        coverage_html(c)
+        coverage_html()
 
 
-@task
-def lint(c):
-    check_all([
-        c.run(f'mypy {PACKAGE_NAME}', warn=True, hide='out'),
-        c.run(f'flake8 {PACKAGE_NAME}', warn=True, hide='out'),
-        c.run(f'pylint {PACKAGE_NAME}', warn=True, hide='out'),
-        c.run(f'mypy tests', warn=True, hide='out'),
-        c.run(f'flake8 tests', warn=True, hide='out'),
-        c.run(f'pylint tests', warn=True, hide='out'),
-    ])
+@APP.command()
+def lint(files: Optional[List[str]] = typer.Argument(default=None,)):  # noqa: B008
+    """Run all linters.
+
+    If files is omitted. everything is linted.
+    """
+
+    subject = files if files else EVERYTHING
+
+    execute(['mypy', *subject])
+    execute(['flake8', *subject])
+    execute(['pylint', *subject])
 
 
-@task
-def format(c):
-    c.run(f'black -q {PACKAGE_NAME}')
-    c.run(f'black -q tests')
-    c.run(f'isort -rc -y -q {PACKAGE_NAME}')
-    c.run(f'isort -rc -y -q tests')
+@APP.command()
+def format(  # pylint: disable=redefined-builtin
+    files: Optional[List[str]] = typer.Argument(default=None),  # noqa: B008
+    check: bool = typer.Option(  # noqa: B008
+        default=False, help='Only checks instead of modifying.'
+    ),
+):
+    """Run all formatters.
+
+    If files is omitted. everything is linted.
+    """
+
+    black_check_flag = ['--check'] if check else []
+    isort_check_flag = ['-c'] if check else []
+
+    subject = files if files else EVERYTHING
+
+    execute(['black', '-q', *black_check_flag, *subject])
+    execute(['isort', '-rc', '-y', '-q', *isort_check_flag, *subject])
 
 
-@task
-def format_check(c):
-    check_all([
-        c.run(f'black --check -q {PACKAGE_NAME}', warn=True, hide='out'),
-        c.run(f'black --check -q tests', warn=True, hide='out'),
-        c.run(f'isort -rc -c -q {PACKAGE_NAME}', warn=True, hide='out'),
-        c.run(f'isort -rc -c -q tests', warn=True, hide='out'),
-    ])
+@APP.command()
+def coverage_html():
+    """Generate an html coverage report."""
+    execute(f'coverage html')
 
 
-@task
-def coverage_html(c):
-    c.run(f'coverage html')
+@APP.command()
+def static_checks():
+    """Run all static checks over all code."""
+    lint([])
+    format([], check=True)
 
 
-@task(pre=[lint, format_check])
-def static_checks(_):
-    pass
+@APP.command()
+def all_checks():
+    """Run all checks (static checks and tests) over all code."""
+    static_checks()
+    test()
 
 
-@task(pre=[static_checks, test])
-def all_checks(_):
-    pass
+if __name__ == '__main__':
+    APP()
